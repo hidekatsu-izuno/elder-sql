40f1f8cf516684867c32d9d14bdc88b638db92e8
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2022-12-17 13:52:21 +0900

    Add sqlite3 functions keyword

--- src/sqlite3/sqlite3_parser.ts
+++ src/sqlite3/sqlite3_parser.ts
@@ -113,7 +113,7 @@ export class Sqlite3Lexer extends Lexer {
       { type: TokenType.BindVariable, re: /\?([1-9][0-9]*)?/y },
       { type: TokenType.BindVariable, re: /[$@:#][a-zA-Z_\u8000-\uFFEE\uFFF0-\uFFFD\uFFFF][a-zA-Z0-9_\u8000-\uFFEE\uFFF0-\uFFFD\uFFFF]*/y },
       { type: TokenType.Identifier, re: /[a-zA-Z_\u8000-\uFFEE\uFFF0-\uFFFD\uFFFF][a-zA-Z0-9_\u8000-\uFFEE\uFFF0-\uFFFD\uFFFF]*/y },
-      { type: TokenType.Operator, re: /\|\||<<|>>|<>|[=<>!]=?|[~&|*/%+-]/y },
+      { type: TokenType.Operator, re: /\|\||<<|>>|<>|->>?|[=<>!]=?|[~&|*/%+-]/y },
       { type: TokenType.Error, re: /./y },
     ], options)
 
