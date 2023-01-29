07591ac8175827e107686556d05b74edb0e92f46
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-01-30 06:25:16 +0900

    Improve code

--- src/driver.ts
+++ src/driver.ts
@@ -1,11 +1,4 @@
-import { MssqlDriver } from "./mssql/mssql_driver.js"
-import { MysqlDriver } from "./mysql/mysql_driver.js"
-import { OracleDriver } from "./oracle/oracle_driver.js"
-import { PostgresDriver } from "./postgres/postgres_driver.js"
-import { Sqlite3Driver } from "./sqlite3/sqlite3_driver.js"
-
 export type ElderSqlDriverOptions = {
-  dialect: "sqlite3" | "mysql" | "postgres" | "oracle" | "mssql"
   host?: string
   port?: string
   username?: string
@@ -14,32 +7,37 @@ export type ElderSqlDriverOptions = {
   [key: string]: any
 }
 
+export interface ElderSqlSelecter<P = Record<string, any>, R = Record<string, any>> {
+  get(params: P): R
+
+  list(params: P): R[]
+
+  iterate(params: P): AsyncIterable<R>
+}
+
+export interface ElderSqlExecuter<P = Record<string, any>, R = any> {
+  execute(params: P): R
+}
+
 export abstract class ElderSqlConnection {
-  abstract transaction<T=void>(proc: () => T): Promise<T>
-  abstract close(): Promise<void>
+  abstract select(text: string): ElderSqlSelecter
+  abstract select<P, R>(builder: (con: ElderSqlConnection) => ElderSqlSelecter<P, R>): ElderSqlSelecter<P, R>
+
+  abstract execute(text: string): ElderSqlExecuter
+  abstract execute<P, R>(builder: (con: ElderSqlConnection) => ElderSqlExecuter<P, R>): ElderSqlExecuter<P, R>
+}
+
+export declare type ElderSqlTransactionOptions = {
 }
 
 export abstract class ElderSqlDriver {
-  static connect(options: ElderSqlDriverOptions) {
-    if (options.dialect === 'sqlite3') {
-      return new Sqlite3Driver(options).connect()
-    } else if (options.dialect === 'mysql') {
-      return new MysqlDriver(options).connect()
-    } else if (options.dialect === 'postgres') {
-      return new PostgresDriver(options).connect()
-    } else if (options.dialect === 'oracle') {
-      return new OracleDriver(options).connect()
-    } else if (options.dialect === 'mssql') {
-      return new MssqlDriver(options).connect()
-    } else {
-      throw new TypeError(`Unknown dialect: ${options.dialect}`)
-    }
-  }
-
   constructor(
     public options: ElderSqlDriverOptions
   ) {
   }
 
-  abstract connect(): Promise<ElderSqlConnection>
+  abstract transaction<T=void>(fn: (con: ElderSqlConnection) => Promise<T>): Promise<T>
+  abstract transaction<T=void>(options: ElderSqlTransactionOptions, fn: (con: ElderSqlConnection) => Promise<T>): Promise<T>
+
+  abstract close(): Promise<void>
 }
