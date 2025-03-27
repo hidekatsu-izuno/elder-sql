d3cb84b8ae8b640dd162f647792ad68807524677
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2025-03-28 07:30:00 +0900

    Add a CstBuilder

--- src/parser.ts
+++ src/parser.ts
@@ -1,44 +1,6 @@
 import { Element, Text } from "domhandler";
-import { appendChild, replaceElement, textContent } from "domutils";
+import { appendChild, replaceElement } from "domutils";
 import type { Lexer, Token } from "./lexer.js";
-
-export abstract class SyntaxElement extends Element {
-	append(child: Element | string) {
-		if (child instanceof Element) {
-			appendChild(this, child);
-		} else {
-			appendChild(this, new Text(child));
-		}
-		return this;
-	}
-
-	replaceWith(replacement: Element) {
-		replaceElement(this, replacement);
-		return this;
-	}
-
-	textContent() {
-		return textContent(this);
-	}
-}
-
-export class SyntaxNode extends SyntaxElement {
-	constructor(type: string, attribs: { [name: string]: string } = {}) {
-		super("node", { ...attribs, type });
-	}
-}
-
-export class SyntaxToken extends SyntaxElement {
-	constructor(type: string, attribs: { [name: string]: string } = {}) {
-		super("token", { ...attribs, type });
-	}
-}
-
-export class SyntaxTrivia extends SyntaxElement {
-	constructor(type: string, attribs: { [name: string]: string } = {}) {
-		super("trivia", { ...attribs, type });
-	}
-}
 
 export abstract class Parser {
 	lexer: Lexer;
@@ -56,16 +18,106 @@ export abstract class Parser {
 		return this.parseTokens(tokens);
 	}
 
-	abstract parseTokens(tokens: Token[]): SyntaxNode;
+	abstract parseTokens(tokens: Token[]): Element;
 }
 
 export class AggregateParseError extends Error {
-	node: SyntaxNode;
+	node: Element;
 	errors: Error[];
 
-	constructor(node: SyntaxNode, errors: Error[], message: string) {
+	constructor(node: Element, errors: Error[], message: string) {
 		super(message);
 		this.node = node;
 		this.errors = errors;
+	}
+}
+
+export class CstBuilder {
+	private _root: Element;
+	private _current: Element;
+
+	constructor(type: string, value?: string | number | boolean) {
+		const elem = new Element("node", {
+			type,
+			...(value != null ? { value: value.toString() } : {}),
+		});
+		this._root = elem;
+		this._current = this._root;
+	}
+
+	get root() {
+		return this._root;
+	}
+
+	get current() {
+		return this._current;
+	}
+
+	start(type: string, value?: string | number | boolean) {
+		const elem = new Element("node", {
+			type,
+			...(value != null ? { value: value.toString() } : {}),
+		});
+		appendChild(this._current, elem);
+		this._current = elem;
+		return this;
+	}
+
+	wrapWith(type: string, value?: string | number | boolean) {
+		const elem = new Element("node", {
+			type,
+			...(value != null ? { value: value.toString() } : {}),
+		});
+		replaceElement(this._current, elem);
+		appendChild(elem, this._current);
+		this._current = elem;
+		return this;
+	}
+
+	value(value: string | number | boolean) {
+		this.current.attribs.value = value.toString();
+	}
+
+	token(token: Token) {
+		const elem = new Element("token", {
+			type: token.type.name,
+			...(token.keyword != null ? { value: token.keyword.name } : {}),
+		});
+		for (const skip of token.preskips) {
+			const skipToken = new Element("trivia", {
+				type: token.type.name,
+				...(token.keyword != null ? { value: token.keyword.name } : {}),
+			});
+			if (skip.text) {
+				appendChild(elem, new Text(token.text));
+			}
+			appendChild(elem, skipToken);
+		}
+		if (token.text) {
+			appendChild(elem, new Text(token.text));
+		}
+		for (const skip of token.postskips) {
+			const skipToken = new Element("trivia", {
+				type: token.type.name,
+				...(token.keyword != null ? { value: token.keyword.name } : {}),
+			});
+			if (skip.text) {
+				appendChild(elem, new Text(token.text));
+			}
+			appendChild(elem, skipToken);
+		}
+		if (!this._current) {
+			throw new Error("Parent node is required.");
+		}
+		appendChild(this._current, elem);
+		return token;
+	}
+
+	end() {
+		if (!(this._current.parent instanceof Element)) {
+			throw new Error(`Parent node is required: ${this._current}`);
+		}
+		this._current = this._current.parent;
+		return this;
 	}
 }
